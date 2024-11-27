78dcccf4b4d71f193d549694d96b3263653b57a3
Daniel Rendon <115592743+SrryBro@users.noreply.github.com>
2024-11-27 07:30:14 -0400

    Crear-Auditoria

--- Front/gulpfile.js
+++ Front/gulpfile.js
@@ -1,5 +1,5 @@
-import path from 'path'
-import fs from 'fs' 
+// import path from 'path'
+// import fs from 'fs' 
 import { src, dest, watch, series } from "gulp";
 import * as dartSass from "sass"; 
 import gulpSass from "gulp-sass";
